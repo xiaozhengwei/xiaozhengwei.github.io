e4719a8d16a173b81ad2a31e45f3fc1039482df3
xiaozhengwei <15226569565@163.com>
2021-03-24 17:20:33 +0800

    学习使我快乐

---
+++ example/Java/BigDecimal/src/BigDecimalTest.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+
+public class BigDecimalTest {
+
+    public static void main(String[] args) {
+        BigDecimal bigDecimal=new BigDecimal(0.1);
+        BigDecimal bigDecimal1=new BigDecimal("0.1");
+        BigDecimal bigDecimal2=new BigDecimal(String.valueOf(0.1));
+        Double d= 0.1;
+        BigDecimal bigDecimal3=new BigDecimal(d.toString());
+        while(true){
+
+        }
+    }
+}
