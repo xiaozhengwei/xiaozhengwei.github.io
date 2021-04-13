4ca38af1c7188f71326d20a388b6b5c95241fd3a
闫正伟 <15226569565@163.com>
2021-04-13 18:56:40 +0800

    学习使我快乐

--- example/Go/src/utunan.com/function/main.go
+++ example/Go/src/utunan.com/function/main.go
@@ -5,16 +5,20 @@ import (
 )
 
 func main()  {
-	happy("你好")
+  num1:=crazy("你好")
+  num2,num3:=happy("你好呀")
+  fmt.Println(num1,num2,num3)
+  floatnum:=float32(21)
+  fmt.Println(floatnum)
 }
 
 
 func happy(s string) (int,int) {
-	fmt.Print(s)
+	fmt.Println(s)
 	return 0 ,1
 }
 
 func crazy(s string) int {
-	fmt.Print(s)
+	fmt.Println(s)
 	return 0
 }
